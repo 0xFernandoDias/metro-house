2e47bf6c0df71236209727feef8245d40937e218
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2023-03-23 20:24:26 -0300

    Chakra UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 "use client"
-
+import { Button } from "@chakra-ui/react"
 import { useGlobalContext } from "./context/store"
 
 export default function Home() {
 	const { count, increment } = useGlobalContext()
 
 	return (
-		<div>
-			<h1>Metro House</h1>
-			<p>Count: {count}</p>
-			<button type="button" onClick={increment}>
-				Increment
-			</button>
-		</div>
+		<>
+			<title>Metro House</title>
+			<div>
+				<h1>Metro House</h1>
+				<p>Count: {count}</p>
+				<Button colorScheme="blue" onClick={increment}>
+					Increment
+				</Button>
+			</div>
+		</>
 	)
 }
