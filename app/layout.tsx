d62c0c4a90f266ced68a267ae8ce0b7a7941bd8f
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2023-03-25 19:43:20 -0300

    Checkpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<ThirdwebProvider activeChain="mumbai">
+				<ThirdwebProvider activeChain="polygon">
 					<LensProvider config={lensConfig}>
 						<GlobalContextProvider>
 							<CacheProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
-// @ts-ignore
-import { useGlobalContext } from "./context/store"
+// import { useGlobalContext } from "./context/store"
 import { ConnectWallet } from "@thirdweb-dev/react"
 import {
 	useExplorePublications,
@@ -11,53 +10,7 @@ import {
 	ContentPublicationFragment,
 	useEncryptedPublication,
 } from "@lens-protocol/react-web"
-import { Button } from "@chakra-ui/react"
-
-export default function Home() {
-	const { count, increment } = useGlobalContext()
-	const {
-		data: publications,
-		loading,
-		hasMore,
-		next,
-	} = useExplorePublications({
-		sortCriteria: PublicationSortCriteria.Latest,
-		publicationTypes: [PublicationTypes.Post],
-	})
-
-	if (loading) {
-		return <div>Loading...</div>
-	}
-
-	return (
-		<>
-			<title>Metro House</title>
-			<div>
-				<h1>Metro House</h1>
-				<p>Count: {count}</p>
-				{/* @ts-ignore */}
-				<Button colorScheme="blue" onClick={increment}>
-					Increment
-				</Button>
-			</div>
-			<div>
-				{publications?.map((publication: AnyPublicationFragment) => {
-					return (
-						<Content
-							key={publication.id}
-							publication={
-								isMirrorPublication(publication)
-									? publication.mirrorOf
-									: publication
-							}
-						/>
-					)
-				})}
-				<ConnectWallet />
-			</div>
-		</>
-	)
-}
+import { Flex, Text } from "@chakra-ui/react"
 
 const Content = ({
 	publication,
@@ -68,5 +21,55 @@ const Content = ({
 		publication,
 	})
 
-	return <p>{data.metadata.content}</p>
+	if (isPending) {
+		return <div>Loading...</div>
+	}
+
+	return (
+		<Flex direction="column" gap="14px">
+			<Text fontWeight="bold">@{data.profile.handle}</Text>
+			<Text>{data.metadata.content}</Text>
+		</Flex>
+	)
+}
+
+export default function Home() {
+	// const { count, increment } = useGlobalContext()
+
+	const {
+		data: publications,
+		loading,
+		hasMore,
+		next,
+	} = useExplorePublications({
+		sortCriteria: PublicationSortCriteria.TopCommented,
+		publicationTypes: [PublicationTypes.Post],
+	})
+
+	if (loading) {
+		return <div>Loading...</div>
+	}
+
+	return (
+		<>
+			<title>Metro House</title>
+			{/* @ts-ignore */}
+			<Flex direction="column" gap="24px">
+				{publications?.map((publication: AnyPublicationFragment) => {
+					return (
+						<div key={publication?.id}>
+							<Content
+								publication={
+									isMirrorPublication(publication)
+										? publication.mirrorOf
+										: publication
+								}
+							/>
+						</div>
+					)
+				})}
+				<ConnectWallet />
+			</Flex>
+		</>
+	)
 }
